63137d5cb466998d2c4934b32b42f6ca3fb05564
Arber Islamaj <arber-islamaj@hotmail.com>
2024-11-21 08:39:25 +0100

    After creating the database on MySQL server, created the database connection, to connect the project with the database through db.php file.

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+// db.php database connection 
+$host = 'localhost';
+$dbname = 'sports-event-calendar';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
