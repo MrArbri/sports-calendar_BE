93b6e2e7cb2cc8d3f3c88f29082243ed9966ba0b
Arber Islamaj <arber-islamaj@hotmail.com>
2024-11-21 09:03:03 +0100

    Created functions.php to implement backend functions

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+require 'db.php';
+
+function getAllEvents($pdo)
+{
+    $query = "
+    SELECT
+        e.id AS event_id,
+        e.description,
+        e.date_time,
+        s.name AS sport_name,
+        v.name AS venue_name
+    FROM Events e
+    JOIN Sports s ON e.sport_id = s.id
+    LEFT JOIN Venues v ON e.venue_id = v.id
+    ORDER BY e.date_time ASC";
+
+    $stmt = $pdo->query($query);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function addEvent($pdo, $sport_id, $description, $venue_id, $date_time)
+{
+    $query = "
+    INSERT IN TO Events (sport_id, description, venue_id, date_time)
+    VALUES (:sport_id, :description, :venue_id, :date_time)";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([
+        ':sport_id' => $sport_id,
+        ':description' => $description,
+        ':venue_id' => $venue_id,
+        ':date_time' => $date_time
+    ]);
+    return $pdo->lastInsertId();
+}
